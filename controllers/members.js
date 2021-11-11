3f0828122044385f27ae474d07da67fa1c3330d1
Nenad Skocic <nenad.skocic@gmail.com>
2021-11-10 23:28:10 -0500

    Username pulled from session for article creation.

--- controllers/members.js
+++ controllers/members.js
@@ -5,20 +5,20 @@ const ArticlesModel = require('../models/articles.js');
 // Display the members page
 router.get("/", function(req, res)
 {
-  res.render("members", req.TPL);
+    res.render("members", req.TPL);
 });
 
 // Create an article if the form has been submitted
 router.post("/create", async function(req, res)
 {
+    /** 
+     *  Create the article using the model method, pass req.body as a parameter since it contains the title and content data... the author is hardcoded
+     *  username displayed on article creation changed to the username currently logged in the session.
+    */
+    await ArticlesModel.createArticle(req.body, req.session.username);
 
-  // Create the article using the model method, pass req.body as a parameter
-  // since it contains the title and content data... the author is hardcoded
-  // to "bob" for now, this should be whichever user is logged-in
-  await ArticlesModel.createArticle(req.body,"bob");
-
-  req.TPL.message = "Article successfully created!";
-  res.render("members", req.TPL);
+    req.TPL.message = "Article successfully created!";
+    res.render("members", req.TPL);
 
 });
 
