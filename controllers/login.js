05e2c48104726925c54a4567cc3de0c052fdea36
Nenad Skocic <nenad.skocic@gmail.com>
2021-11-10 23:22:39 -0500

    Login authentication/error handling /w re-directs.

--- controllers/login.js
+++ controllers/login.js
@@ -1,5 +1,12 @@
+/*
+    "StAuth10222: I Nenad Skocic, 000107650 certify that this material is my original work. No other person's work has been used 
+    without due acknowledgement. I have not made my work available to anyone else."
+
+    2) Login Login (Lines 27 - 64)
+*/
+
 const express = require('express');
-var router = express.Router()
+var router = express.Router();
 
 // Displays the login page
 router.get("/", async function(req, res)
@@ -12,25 +19,50 @@ router.get("/", async function(req, res)
     res.render("login", req.TPL);
 });
 
-// Attempts to login a user
-// - The action for the form submit on the login page.
-router.post("/attemptlogin", async function(req, res)
+/**
+ *  2) Login Login
+ *  Attempts to login a user.
+ *  - The action for the form submit on the login page.
+ */
+router.post("/attemptlogin", async (req, res) =>
 {
-    // is the username and password OK?
-    if (req.body.username == "bob" &&
-        req.body.password == "test")
-    {
-    // set a session key username to login the user
-    req.session.username = req.body.username;
+    // Error handling in async/await.
+    try {
+        // Sets username and password to the input provided on the login page.
+        const username = req.body.username;
+        const password = req.body.password;
 
-    // re-direct the logged-in user to the members page
-    res.redirect("/members");
-    }
-    else
-    {
-    // if we have an error, reload the login page with an error
-    req.session.login_error = "Invalid username and/or password!";
-    res.redirect("/login");
+        // Finds the exact username in the database if the input on the login form matches an existing username;
+        const user = await db.all("SELECT * FROM Users WHERE username = '" + username + "'");
+        
+        let pass = "";
+        let level = "";
+    
+        // Loop through the user array and pull the password and level values.
+        for(i in user) {
+            pass = user[i].password;
+            level = user[i].level;
+        }
+
+        // Checks if password inputted from user matches in database for specific username and checks the level of access provided to that username.
+        if(password == pass && level == "member") {
+            // Set a session key username to login the user
+            req.session.username = username;
+            req.session.level = level;
+            // Re-direct the logged-in user to the members page
+            res.redirect("/members");
+        } 
+        if (password == pass && level == "editor") {
+            req.session.username = username;   
+            req.session.level = level;
+            res.redirect("/editors");          
+        } else {
+            // If we have an error, reload the login page with an error
+            req.session.login_error = "Invalid username and/or password!";
+            res.redirect("/login");
+        }
+    } catch(error) {
+        console.log(error);
     }
 });
 
@@ -39,6 +71,8 @@ router.post("/attemptlogin", async function(req, res)
 // is logged in, re-directs them to the home page.
 router.get("/logout", async function(req, res)
 {
+    // Destroy the session key level.
+    delete(req.session.level);
     delete(req.session.username);
     res.redirect("/home");
 });
